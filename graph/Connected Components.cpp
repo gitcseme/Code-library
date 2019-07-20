169d9fecddd1903a214b9b49dedb4b709f216b91
kashuvo <shuvo_CSE23@yahoo.com>
2019-07-21 03:24:59 +0600

    connected component using dfs

---
+++ graph/Connected Components.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define FI freopen ("inp.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+using namespace std;
+
+int g[1000];
+bool vis[1000];
+vector <int> adj[1000];
+
+void DFS(int u) {
+    vis[u] = true;
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            DFS(v);
+        }
+    }
+}
+
+int main() {
+    FI;
+    memset(vis, false, sizeof(vis));
+    int V, E, u, v;
+
+    cin >> V >> E;
+    for (int i = 0; i < E; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int numOfComponent = 0;
+    for (int i = 1; i <= E; i++) {
+        if (!vis[i]) {
+            ++numOfComponent;
+            DFS(i);
+        }
+    }
+
+    cout << numOfComponent;
+
+    return 0;
+}
