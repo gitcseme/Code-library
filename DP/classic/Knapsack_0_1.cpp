54462343379eac39bcb921af115253e7d0f342f3
kashuvo <shuvo_CSE23@yahoo.com>
2020-06-01 20:23:45 +0600

    modified & added top-down approach

--- DP/classic/Knapsack_0_1.cpp
+++ DP/classic/Knapsack_0_1.cpp
@@ -1,53 +1,67 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define INF 9999
-
-int knapsack(int i, int sack);
-int p[100];
-int w[100];
-int mem[100][100];
-int path[100][100];
-
-int main() {
-    memset(mem, -1, sizeof(mem));
-    memset(path, -1, sizeof(path));
-
-    int num_of_products, sack_size;
-    scanf("%d %d", &num_of_products, &sack_size);
-
-    for (int i = 0; i < num_of_products; i++)
-        scanf("%d %d", &p[i], &w[i]);
-
-    int max_benifit = knapsack(num_of_products-1, sack_size);
-    printf("%d\n", max_benifit);
-
-    /* Result printing */
-    int i = num_of_products-1, sack = sack_size;
-    while (i >= 0 && sack >= 0) {
-        if (path[i][sack] == 2) {
-            printf("%d %d\n", p[i], w[i]);
-            sack = sack - w[i];
-        }
-        --i;
-    }
-
-    return 0;
-}
+const int maxn = 1005;
+vector<int> p;
+vector<int> w;
+int mem[maxn][maxn];
 
 int knapsack(int i, int sack) {
-    if (sack < 0) return -INF;
-    if (i < 0) return 0;
+    if (i == 0 || sack == 0)
+        return 0;
 
     if (mem[i][sack] != -1) return mem[i][sack];
 
-    int ans1 = knapsack(i-1, sack);
-    int ans2 = p[i] + knapsack(i-1, sack - w[i]); // knapsack(i, sack-w[i]) for taking item i multiple times.
+    if (w[i-1] <= sack)
+        mem[i][sack] = max(p[i-1] + knapsack(i-1, sack-w[i-1]), knapsack(i-1, sack));
+    else
+        mem[i][sack] = knapsack(i-1, sack);
 
-    if(ans1 > ans2) path[i][sack] = 1;
-    else path[i][sack] = 2;
+    return mem[i][sack];
+}
 
-    return mem[i][sack] = max(ans1, ans2);
+int main() {
+    memset(mem, -1, sizeof(mem));
+
+    int n = 4;
+    int sack = 7;
+
+    w = {1, 3, 4, 5};
+    p = {2, 4, 5, 7};
+
+    cout << knapsack(n, sack) << "\n";
+
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= sack; ++j) {
+            cout << mem[i][j] << " ";
+        }
+        puts("");
+    }
+    puts("---------------------------------\n");
+
+    // TopDown
+    int dp[n+1][sack+1];
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= sack; ++j) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;
+                continue;
+            }
+            if (w[i-1] <= j)
+                dp[i][j] = max(p[i-1] + dp[i-1][j-w[i-1]], dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    cout << dp[n][sack] << "\n";
+
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= sack; ++j) {
+            cout << dp[i][j] << " ";
+        }
+        puts("");
+    }
+
+    return 0;
 }
